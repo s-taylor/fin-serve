fdfe73fff33f468c8ff66f80826a9b15844bcb53
Drew Showalter <maelstroswim@gmail.com>
2016-09-29 15:32:50 +1000

    Better path binding to env var.

--- www.js
+++ www.js
@@ -4,7 +4,7 @@ const http = require('http');
 const serveStatic = require('serve-static');
 const finalhandler = require('finalhandler');
 
-const STATIC_DIR = process.env.STATIC_DIR || `${__dirname}/dist`;
+const STATIC_DIR = `${process.cwd()}/${process.env.STATIC_DIR || 'dist'}`;
 const HOST_PORT = process.env.HOST_PORT || 8000;
 
 const serve = serveStatic(STATIC_DIR);
