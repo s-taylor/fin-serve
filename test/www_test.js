7fe0797794fb44faf01d48eb1b28d1714efae7a7
Drew Showalter <maelstroswim@gmail.com>
2016-09-29 15:53:38 +1000

    Updates tests to match new architecture.

--- test/www_test.js
+++ test/www_test.js
@@ -2,10 +2,11 @@ const http = require('http');
 const fs = require('fs');
 
 const asset = fs.readFileSync('test/fixtures/index.html').toString();
+const jsAsset = fs.readFileSync('test/fixtures/app.js').toString();
 
 describe('/', () => {
   it('should return 200', (done) => {
-    http.get('http://localhost:8000', (res) => {
+    http.get('http://localhost:8000/', (res) => {
       res.statusCode.must.equal(200);
       done();
     });
@@ -25,11 +26,41 @@ describe('/', () => {
       });
     });
   });
+  it('should return js assets', (done) => {
+    http.get('http://localhost:8000/app.js', (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        data.must.equal(jsAsset);
+        done();
+      });
+    });
+  });
 });
 
 describe('/whatever', () => {
-  it('should return 200 & static assets on all routes', (done) => {
+  it('should return 200, and index.html on incorrect path', (done) => {
     http.get('http://localhost:8000/flingledorp', (res) => {
+      res.statusCode.must.equal(200);
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        data.must.equal(asset);
+        done();
+      });
+    });
+  });
+
+  it('should return 200, and index.html on incorrect file types', (done) => {
+    http.get('http://localhost:8000/app.js.map', (res) => {
       res.statusCode.must.equal(200);
       let data = '';
 
