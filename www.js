2fd6d43b701e82cf16a1db44ce2dd5375067e062
Drew Showalter <maelstroswim@gmail.com>
2017-06-29 10:53:13 +1000

    :sparkles: allows you to specify a meta api url header tag at runtime.

--- www.js
+++ www.js
@@ -1,20 +1,32 @@
 #!/usr/bin/env node
-
+const fs = require('fs');
 const http = require('http');
 const serveStatic = require('serve-static');
 const finalhandler = require('finalhandler');
 
 const STATIC_DIR = `${process.cwd()}/${process.env.STATIC_DIR || 'dist'}`;
 const HOST_PORT = process.env.HOST_PORT || 8000;
+const META_API_LOCATION = process.env.API_URL || 'http://localhost:3005';
+const metaString = `<meta name="API_URL" content="${META_API_LOCATION}">`;
+
+const normalIndex = fs.readFileSync(`${STATIC_DIR}/index.html`, 'utf8');
+const metaAmmendedIndex = !normalIndex ? '' :
+  normalIndex.split('\n').reduce((full, line) => {
+    full += `${line}\n`;
+    if (line.includes('<head>')) full += `  ${metaString}\n`;
+    return full;
+  }, '');
 
 const serve = serveStatic(STATIC_DIR);
+console.log('Serving your files now!');
 
 const server = http.createServer((req, res) => {
   if (!req.url.match(/\.(html|css$|js$|json|webapp|cache|jpg|svg|png|ico|txt|eot|ttf|woff)/g)) {
     req.url = '/';
+    res.end(metaAmmendedIndex);
+  } else {
+    serve(req, res, finalhandler(req, res));
   }
-
-  serve(req, res, finalhandler(req, res));
 });
 
 server.listen(HOST_PORT);
